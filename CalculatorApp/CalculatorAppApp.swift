b3beb498a2d4b1fdedf4bea449b86b3a82e5c28e
Sourav Jangra <imsouravjangra@gmail.com>
2025-10-10 20:31:55 +0530

    Initial Commit

---
+++ CalculatorApp/CalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorAppApp.swift
+//  CalculatorApp
+//
+//  Created by Sourav Jangra on 10/10/25.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
